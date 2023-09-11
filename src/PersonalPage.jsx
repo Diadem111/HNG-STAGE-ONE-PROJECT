e1f22d67cabc6fb72eaeb584c2a0dd9657f778da
wahab omodolapo <dolwithneybright1@gmail.com>
2023-09-11 17:48:52 +0100

    Done

--- src/PersonalPage.jsx
+++ src/PersonalPage.jsx
@@ -19,24 +19,26 @@ setCurrentUTCTime(currentUTCTime);
     }, [])
     
   return (
-    <div  className='p-4'>
+    <div  className='p-4 items-center text-center '>
         {/* Slack Name */}
-        <h1 data-testid="slackUserName" className='text-3xl'>Dolapo Wahab</h1>
+        <h1 data-testid="slackUserName" className='text-3xl font-zodiak '>Dolapo Wahab</h1>
     
     {/* Slack Picture */}
-    <img  src="" alt="Dolapo Wahab" data-testid="SlackDisplayImage" className='w-32 h-32 mt-4'/>
    
+   <img  src="../dolapo.JPG" alt="Dolapo Wahab" data-testid="SlackDisplayImage" className='w-50 h-40 mt-4  mx-auto rounded-md  '/>
+  
    {/* current day of the week */}
-   <p data-testid="currentDayOfTheWeek" className='text-lg mt-4'>Current Day: {currentDay}</p>
+   <p data-testid="currentDayOfTheWeek" className='text-lg mt-4 font-zodiak'>Current Day: {currentDay}</p>
     
     {/* current UTC Time */}
-    <p data-testid="currentUTCTime" className='text-lg'>Current UTC Time (milliseconds) : {currentUTCTime}</p>
+    <p data-testid="currentUTCTime" className='text-lg font-zodiak'>Current UTC Time (milliseconds) : {currentUTCTime}</p>
     
 
     {/* Track */}
-    <p data-testid="myTrack" className='text-lg mt-4'>Track:Frontend</p>
+    <p data-testid="myTrack" className='text-lg mt-4 font-zodiak '>Track:Frontend</p>
     
-    git
+    {/* gitHub Url */}
+    <a href='https://github.com/Diadem111/INTERN_PROJECT' data-testid="githubURL" className='text-blue-500 mt-4' target='_blank' rel="noopener noreferrer">GutHub Repository</a>
     </div>
   )
 }
